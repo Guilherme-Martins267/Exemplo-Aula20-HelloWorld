f2c1b9eaad8faef2e6381d9d89f5270cbee6db24
Guilherme-Martins267 <guihbm.m@gmail.com>
2020-04-19 20:31:45 -0300

    Exemplo de TomCat.

---
+++ src/main/java/Martins/Guilherme/ExemploHelloWorld/ExemploHelloWorldApplication.java
@@ -0,0 +1,15 @@
+package Martins.Guilherme.ExemploHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploHelloWorldApplication.class, args);
+
+		System.out.println("Exemplo com tomCat");
+	}
+
+}
